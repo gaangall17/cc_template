54b3fe81bd68d06a957d5651f43c108511f88655
ggallardo <ga.gallardo95@gmail.com>
2022-03-20 14:42:13 -0500

    Added hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from hooks.pre_gen_project import MESSAGE_COLOR, RESET_ALL
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a git repository...{RESET_ALL}")
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call(['conda', 'env', 'create','--file','environment.yml'])
+
+print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
