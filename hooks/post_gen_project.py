4db361ee4e84f9a1d403b3e3853e574d356339fa
ggallardo <ga.gallardo95@gmail.com>
2022-03-27 13:47:59 -0500

    modules added

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,6 +8,6 @@ print(f"Initializing a git repository...{RESET_ALL}")
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-subprocess.call(['conda', 'env', 'create','--file','environment.yml'])
+subprocess.call(['conda', 'env', 'create','--file','{{ cookiecutter.project_slug }}/environment.yml'])
 
 print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
